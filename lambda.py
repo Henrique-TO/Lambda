e0459aac057b351bfc42acbc33eacb1bd3a9577f
Henrique-TO <144277732+Henrique-TO@users.noreply.github.com>
2023-10-05 16:08:24 -0300

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,14 @@
+dobro= lambda x: x*2
+# print(dobro(100))
+
+soma= lambda x,y: x + y
+# print(soma(5,5))
+
+par= lambda x: x%2==0
+# print(par(4))
+
+maiusculo= lambda x: x.upper()
+# print(maiusculo('bah'))
+
+calc= lambda x,y,z: x*y*z
+# print(calc(5,5,5))
